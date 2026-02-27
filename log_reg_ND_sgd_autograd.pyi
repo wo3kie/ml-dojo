e3cc8ccb0cbe7a17c2513f22f1375878b6dd805e
wo3kie <wo3kie@outlook.com>
2026-02-27 12:58:42 +0100

    Update log_reg_ND_sgd_autograd

---
+++ log_reg_ND_sgd_autograd.pyi
@@ -0,0 +1,6 @@
+# Stub file for log_reg_ND_sgd_autograd.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+
+def logistic_regression_sgd_autograd(X, y, epochs=2000, lr=0.1) -> tuple[float, callable]: ...
