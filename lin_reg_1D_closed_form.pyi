d5704b4f59643a9ada918da325aa1c76ab72d420
wo3kie <wo3kie@outlook.com>
2026-02-27 22:32:38 +0100

    Update lin_reg_1D_closed_form

--- lin_reg_1D_closed_form.pyi
+++ lin_reg_1D_closed_form.pyi
@@ -1,6 +1,6 @@
-# Stub file for lin_reg_1D_closed_formula.ipynb
+# Stub file for lin_reg_1D_closed_form.ipynb
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
 
-def linear_regression_1d_cf(x, y): ...
+def linear_regression_1d_cf(x: Tensor, y: Tensor) -> tuple[float, float]: ...
