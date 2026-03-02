73dfc6abe88146f4700a08e130960b901dd7375f
wo3kie <wo3kie@outlook.com>
2026-03-02 20:50:06 +0100

    Update log_reg_2C_sgd_autograd

--- log_reg_2C_sgd_autograd.pyi
+++ log_reg_2C_sgd_autograd.pyi
@@ -1,6 +1,7 @@
-# Stub file for log_reg_ND_sgd_autograd.ipynb
+# Stub file for log_reg_2C_sgd_autograd.ipynb
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
 
 def logistic_regression_2c_sgd_autograd(X, y, epochs=2000, lr=0.1) -> tuple[float, callable]: ...
