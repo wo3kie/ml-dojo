5391793fc41ca2064ff6434b9aead9b5aa375761
wo3kie <wo3kie@outlook.com>
2026-03-02 20:50:06 +0100

    Update cross_entropy

--- cross_entropy.pyi
+++ cross_entropy.pyi
@@ -2,6 +2,7 @@
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
 
 def _cross_entropy_d(dist_x, dist_y): ...
 def cross_entropy(iterable_x, iterable_y, count=False, norm=False): ...
