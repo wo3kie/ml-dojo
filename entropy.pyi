3c598c081a10c21dd058225c83d9d972684a238d
wo3kie <wo3kie@outlook.com>
2026-03-02 20:50:06 +0100

    Update entropy

--- entropy.pyi
+++ entropy.pyi
@@ -2,5 +2,6 @@
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
 
 def entropy(iterable, count=False, norm=False): ...
