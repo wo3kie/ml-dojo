8bfef820339c7a68148b12c9cd2d50eee664aaba
wo3kie <wo3kie@outlook.com>
2026-03-08 20:20:27 +0100

    Update log_reg_NC_sgd_gradient

---
+++ log_reg_NC_sgd_gradient.pyi
@@ -0,0 +1,7 @@
+# Stub file for log_reg_NC_sgd_gradient.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
+
+def logistic_regression_nc_sgd_gradient(X: Tensor, Y: Tensor, epochs=2000, lr=0.1) -> tuple[float, callable]: ...
