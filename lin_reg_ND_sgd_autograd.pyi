b8fc00bd6d77095dee109cdbd43c2fc4f10e4494
wo3kie <wo3kie@outlook.com>
2026-03-08 20:20:27 +0100

    Update lin_reg_ND_sgd_autograd

---
+++ lin_reg_ND_sgd_autograd.pyi
@@ -0,0 +1,7 @@
+# Stub file for lin_reg_ND_sgd_autograd.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
+
+def linear_regression_nd_sgd_autograd(X: Tensor, Y: Tensor, epochs=1000, lr=0.1) -> tuple[Tensor, Tensor, Tensor]: ...
