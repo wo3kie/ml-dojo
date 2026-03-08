edf3788b3a6d7c99f9731aa18146933f145e689b
wo3kie <wo3kie@outlook.com>
2026-03-08 20:20:27 +0100

    Update lin_reg_ND_sgd_gradient

---
+++ lin_reg_ND_sgd_gradient.pyi
@@ -0,0 +1,7 @@
+# Stub file for lin_reg_ND_sgd_gradient.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
+
+def linear_regression_nd_sgd_gradient(X: Tensor, Y: Tensor, epochs=1000, lr=0.1) -> tuple[float, Tensor, Tensor]: ...
