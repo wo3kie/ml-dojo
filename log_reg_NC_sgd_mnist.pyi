d733b17132829a94845da50accf219f2a1e62a05
wo3kie <wo3kie@outlook.com>
2026-03-10 20:22:42 +0100

    Update log_reg_NC_sgd_mnist

--- log_reg_NC_sgd_mnist.pyi
+++
@@ -1,5 +0,0 @@
-# Stub file for log_reg_NC_sgd_mnist.ipynb
-# Auto-generated from notebook code cells
-
-from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
-from torch import Tensor
