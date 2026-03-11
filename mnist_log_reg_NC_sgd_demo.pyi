c17a9513b6874f984055f80e87037c838e9d9692
wo3kie <wo3kie@outlook.com>
2026-03-11 08:18:29 +0100

    Update mnist_log_reg_NC_sgd_demo

---
+++ mnist_log_reg_NC_sgd_demo.pyi
@@ -0,0 +1,5 @@
+# Stub file for mnist_log_reg_NC_sgd_weights.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+from torch import Tensor
