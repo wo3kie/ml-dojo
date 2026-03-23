491a63b93692fe691a7d5da85bf88db9c26bb4f8
wo3kie <wo3kie@outlook.com>
2026-03-23 21:53:54 +0100

    Update lin_reg_1D_normal_equation

--- lin_reg_1D_normal_equation.pyi
+++ lin_reg_1D_normal_equation.pyi
@@ -2,6 +2,6 @@
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
-from torch import Tensor
+import torch as tr
 
-def linear_regression_1d_ne(X: Tensor, Y: Tensor) -> tuple[float, float]: ...
+def linear_regression_1d_ne(X: tr.Tensor, Y: tr.Tensor) -> tuple[float, float]: ...
