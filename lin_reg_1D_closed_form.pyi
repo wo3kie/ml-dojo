f664ccd3d2bff012b7c26e80a92f2dc240ca016d
wo3kie <wo3kie@outlook.com>
2026-03-23 21:53:54 +0100

    Update lin_reg_1D_closed_form

--- lin_reg_1D_closed_form.pyi
+++ lin_reg_1D_closed_form.pyi
@@ -2,6 +2,6 @@
 # Auto-generated from notebook code cells
 
 from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
-from torch import Tensor
+import torch as tr
 
-def linear_regression_1d_cf(X: Tensor, Y: Tensor) -> tuple[float, float]: ...
+def linear_regression_1d_cf(X: tr.Tensor, Y: tr.Tensor) -> tuple[float, float]: ...
