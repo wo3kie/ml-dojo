2e65e15abc7e68ac472237d0692f9933ce10d848
wo3kie <wo3kie@outlook.com>
2026-03-23 21:53:54 +0100

    Update linear

---
+++ linear.pyi
@@ -0,0 +1,7 @@
+# Stub file for linear.ipynb
+# Auto-generated from notebook code cells
+
+from typing import Any, Callable, Iterable, List, Optional, Tuple, Union
+import torch as tr
+
+def linear(x: tr.Tensor, W: tr.Tensor, b: tr.Tensor) -> tr.Tensor: ...
